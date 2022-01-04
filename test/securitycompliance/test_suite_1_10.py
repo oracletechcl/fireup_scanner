ef953dbe870ad7bc2a92c5d998a26888c410c24d
Matt Mcloughlin <mattmcl2000@gmail.com>
2022-01-04 09:14:35 +0000

    Adds test suite
    Updates testing values

--- test/securitycompliance/test_suite_1_10.py
+++ test/securitycompliance/test_suite_1_10.py
@@ -36,6 +36,6 @@ def test_review_point(capsys):
         debug_with_date(item)
         results_in_fault += 1    
 
-    assert results_in_fault == 138
+    assert results_in_fault == 141
 
     __test_suite_log(capsys)
