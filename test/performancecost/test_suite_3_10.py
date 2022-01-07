781f801e61cd55fa1272a7c74a8926d9d8f2d142
Matt Mcloughlin <mattmcl2000@gmail.com>
2022-01-07 11:17:02 +0000

    Adds test suite
    Updates testing values

--- test/performancecost/test_suite_3_10.py
+++ test/performancecost/test_suite_3_10.py
@@ -4,37 +4,37 @@
 # Description: Main test suite for fireup review tool
 # Dependencies: pytest
 
-from classes.reliabilityresilience.ReplicateData import ReplicateData
+from classes.performancecost.CheckAutoTuning import CheckAutoTuning
 from common.utils.helpers.helper import get_config_and_signer
 from common.utils.formatter.printer import debug_with_date
 from common.utils.statics import Statics
 from common.utils.tokenizer.signer import *
 
   
-
 def __test_suite_log(capsys):
     out, err = capsys.readouterr()
     open("stderr.out", "w").write(err)
     open("stdout.out", "w").write(out)
 
+
 def test_review_point(capsys):     
     
-    result_dictionary = ReplicateData(Statics.__rp_2_17['entry'], 
-    Statics.__rp_2_17['area'], 
-    Statics.__rp_2_17['sub_area'], 
-    Statics.__rp_2_17['review_point'], 
+    result_dictionary = CheckAutoTuning(Statics.__rp_3_10['entry'], 
+    Statics.__rp_3_10['area'], 
+    Statics.__rp_3_10['sub_area'], 
+    Statics.__rp_3_10['review_point'], 
     True, [], [], [], [], 
     get_config_and_signer()[0], 
     get_config_and_signer()[1]
     )
 
     results_in_fault=0
-    dictionary = result_dictionary.analyze_entity(Statics.__rp_2_17['entry'])   
+    dictionary = result_dictionary.analyze_entity(Statics.__rp_3_10['entry'])   
 
-    for item in dictionary[Statics.__rp_2_17['entry']]['findings']:
+    for item in dictionary[Statics.__rp_3_10['entry']]['findings']:
         debug_with_date(item)
         results_in_fault += 1
 
-    assert results_in_fault == 304
+    assert results_in_fault == 195
 
     __test_suite_log(capsys)
