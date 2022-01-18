f951b0b86b18c36047c057e887522057af287d9c
nicolinapieri <nikolinapieri@gmail.com>
2022-01-18 12:31:19 +0000

    Updated SecureDNS.py header

--- classes/securitycompliance/SecureDNS.py
+++ classes/securitycompliance/SecureDNS.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2021 Oracle and/or its affiliates.
 # All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
-# BackupDatabases.py
+# SecureDNS.py
 # Description: Implementation of class SecureDNS based on abstract
 
 from common.utils.helpers.helper import *
