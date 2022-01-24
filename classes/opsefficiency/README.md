62d13ebc8e0c72be5059a0d10eeee06ecd021ac1
dralquinta <dralquinta@gmail.com>
2022-01-24 02:47:54 +0000

    Fixing Error on dictionary for RP 1.24 and 4.5. Fixing issue with missing unitary test entry on removed function debug_with_date() still being called. Implementing RP 4.5 as reported on https://github.com/oraclecloudbricks/fireup/issues/143

--- classes/opsefficiency/README.md
+++ classes/opsefficiency/README.md
@@ -3,5 +3,6 @@
 | Review Point | Concrete Class Implementation                           
 |--------------|---------------------------------------------------------
 | 4.2          | [MetricAlarms.py](MetricAlarms.py) 
+| 4.5          | [CloudGuardEnabled.py](CloudGuardEnabled.py)
 | 4.6          | [ConfigureAuditing.py](ConfigureAuditing.py)   
                                   
--- test/opsefficiency/test_suite_4_5.py
+++ test/opsefficiency/test_suite_4_5.py
@@ -4,37 +4,36 @@
 # Description: Main test suite for fireup review tool
 # Dependencies: pytest
 
-from os import write
-from classes.securitycompliance.CloudGuardMonitor import CloudGuardMonitor
+from classes.opsefficiency.CloudGuardEnabled import CloudGuardEnabled
 from common.utils.helpers.helper import get_config_and_signer
 from common.utils.formatter.printer import debug
 from common.utils.statics import Statics
 from common.utils.tokenizer.signer import *
 
   
-
 def __test_suite_log(capsys):
     out, err = capsys.readouterr()
     open("stderr.out", "w").write(err)
     open("stdout.out", "w").write(out)
 
+
 def test_review_point(capsys):     
     
-    result_dictionary = CloudGuardMonitor(Statics.__rp_1_24['entry'], 
-    Statics.__rp_1_24['area'], 
-    Statics.__rp_1_24['sub_area'], 
-    Statics.__rp_1_24['review_point'], 
+    result_dictionary = CloudGuardEnabled(Statics.__rp_4_5['entry'],
+    Statics.__rp_4_5['area'],
+    Statics.__rp_4_5['sub_area'],
+    Statics.__rp_4_5['review_point'],
     True, [], [], [], [], 
     get_config_and_signer()[0], 
     get_config_and_signer()[1]
     )
 
     results_in_fault=0
-    dictionary = result_dictionary.analyze_entity(Statics.__rp_1_24['entry'])   
-    
-    for item in dictionary[Statics.__rp_1_24['entry']]['findings']:
+    dictionary = result_dictionary.analyze_entity(Statics.__rp_4_5['entry'])
+
+    for item in dictionary[Statics.__rp_4_5['entry']]['findings']:
         debug(item)
-        results_in_fault += 1    
+        results_in_fault += 1
 
     assert results_in_fault == 0
 
