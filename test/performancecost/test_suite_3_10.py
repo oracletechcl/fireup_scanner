3326fb4ec253147d8242787777122643a41f6c23
Matt Mcloughlin <mattmcl2000@gmail.com>
2022-02-07 10:10:27 +0000

    Updates test values #199

--- test/performancecost/test_suite_3_10.py
+++ test/performancecost/test_suite_3_10.py
@@ -35,6 +35,6 @@ def test_review_point(capsys):
         debug(item)
         results_in_fault += 1
 
-    assert results_in_fault == 144
+    assert results_in_fault == 143
 
     __test_suite_log(capsys)
