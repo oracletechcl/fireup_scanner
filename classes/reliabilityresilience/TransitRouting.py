4450b135584ee735f4484edc647bf6133c5debcb
GrzegorzWysopal <wysopalgrzegorz@gmail.com>
2022-02-09 09:02:42 +0000

    debug statements

--- classes/reliabilityresilience/TransitRouting.py
+++ classes/reliabilityresilience/TransitRouting.py
@@ -84,11 +84,9 @@ class TransitRouting(ReviewPoint):
                 if com_region[1] == region.region_key.lower() or com_region[1] == region.region_name.lower() :
                     region_needed = self.config
                     region_needed['region'] = region.region_name
-
+                    debug(region_needed,color = 'cyan')
                     n_client = get_virtual_network_client(region_needed, self.signer)
-                    debug(n_client.base_client.endpoint,color = 'blue')
                     n_client.base_client.endpoint = 'https://vnca-api.' + region.region_name + '.oci.oraclecloud.com'
-                    debug(n_client.base_client.endpoint,color = 'yellow')
                     self.__topologies_with_cpe_connections_objects.append(get_networking_topology_per_compartment(n_client,com_region[0]))
 
         for network_topology in self.__topologies_with_cpe_connections_objects:
